60ea6d78b8878095e8df6ce9d3e40d6ffd760fa5
gechleang <gechleanglysan1211@gmail.com>
2024-10-13 22:41:16 +0700

    Update

--- W1-S2.Dart-Basics/EX-4.dart
+++ W1-S2.Dart-Basics/EX-4.dart
@@ -1,13 +1,20 @@
 void main() {
-  // Map of pizza prices
   const pizzaPrices = {
     'margherita': 5.5,
     'pepperoni': 7.5,
     'vegetarian': 6.5,
   };
-
+  double total=0;
   // Example order
   const order = ['margherita', 'pepperoni', 'pineapple'];
-
-  // Your code
+  
+  for (var entry in pizzaPrices.entries) {
+    for(var i=0;i<order.length;i++){
+        if(entry.key==order[i]){
+      total +=entry.value;
+     }
+    }
+   }
+  print(total);
+  
 }
